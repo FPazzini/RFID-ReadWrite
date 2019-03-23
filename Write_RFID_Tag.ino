dbde81d8e7449f23ee566089fe7fa78596581fc6
FPazzini <fabio.pazzini@icloud.com>
2019-03-23 10:38:05 +0100

    First commit

---
+++ Write_RFID_Tag.ino
@@ -0,0 +1,73 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+#define PN532_SCK  (2)
+#define PN532_MOSI (3)
+#define PN532_SS   (4)
+#define PN532_MISO (5)
+
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+#if defined(ARDUINO_ARCH_SAMD)
+   #define Serial SerialUSB
+#endif
+
+void setup(void) {
+  #ifndef ESP8266
+    while (!Serial);
+  #endif
+  Serial.begin(115200);
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    while (1);
+  }
+  nfc.SAMConfig();
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  // Wait for an ISO1443A type card --> Mifare Classic cards will have a 4 bytes-long uid, Mifare Ultralight 7
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    if (uidLength == 4)
+    {
+      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+	  
+      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+	  
+      if (success)
+      {
+        //Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+        uint8_t data[16];
+
+        // Writing on the tag
+		    memcpy(data, (const uint8_t[]){ 'h', 'a', 'm', 'm', 'e', 'r', 'h', 'e', 'a', 'd', 's', 'h', 'a', 'r', 'k', 0 }, sizeof data);
+        success = nfc.mifareclassic_WriteDataBlock (4, data);
+        
+        if (success)
+        {
+
+          // PRINTING OUT THE DATA FOUND ON THE RFID TAG
+          char* input_cr = (char*) &data;
+          Serial.println(input_cr);
+		  
+          // Wait a bit before reading the card again
+          delay(1000);
+        }
+      }
+    }
+  }
+}
